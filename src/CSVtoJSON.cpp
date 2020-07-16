0971117ef381fbd79dda6d3024b2145d43fb0baa
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-07-16 03:18:35 -0400

    implemented converter

--- src/CSVtoJSON.cpp
+++ src/CSVtoJSON.cpp
@@ -3,17 +3,6 @@
 #include <string>
 #include <vector>
 
-
-std::string get_header(std::string &file_name) {
-	std::ifstream file;
-	file.open(file_name);
-    std::string str_row;
-    if (file.good()) {
-        getline(file, str_row);
-    }
-	return str_row;
-}
-
 std::vector<std::string> split_row_commas(std::string &str_row){
     std::vector<std::string> parsed_row;
     int comma = str_row.find(',');
@@ -26,12 +15,56 @@ std::vector<std::string> split_row_commas(std::string &str_row){
     return parsed_row;
 }
 
-int main()
-{
-    std::string path = "/home/kurt/github/CSVtoJSON/src/PER_data.csv";
-    std::string test = get_header(path);
-    auto parsed = split_row_commas(test);
-    for(int i = 0; i < parsed.size(); i++){
-        std::cout << parsed[i] << std::endl;
+std::vector<std::string> get_header(std::ifstream &file){
+    std::string str_row;
+    if (file.good()) {
+        getline(file, str_row);
     }
+	return split_row_commas(str_row);
+}
+
+bool is_number(std::string &value){
+    for(int i = 0; i < value.length(); i++){
+        if(!isdigit(value[i])){
+            if(value[i] != '.'){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void converter(std::ifstream &infile, std::ofstream &outfile, std::vector<std::string> &header){
+    std::string json_contents = "[\n";
+    std::string str_row;
+    bool header_check = false;
+    while(getline(infile, str_row)){
+        if(!header_check){
+            header_check = true;
+            continue;
+        }
+        std::vector<std::string> parsed_row = split_row_commas(str_row);
+        json_contents.append("\t{\n");
+        for(int i = 0; i < header.size(); i++){
+            json_contents.append("\t\t\"" + header[i] + "\": ");
+            if(is_number(parsed_row[i])){
+                json_contents.append(parsed_row[i] + ",\n");
+            }
+            else{
+                json_contents.append("\"" + parsed_row[i] + "\",\n");
+            }
+        }
+        json_contents.append("\t},\n");
+    }
+    json_contents.append("]");
+    std::cout << json_contents << std::endl;
+}
+
+int main(int argc, char *argv[]){
+    std::ifstream infile;
+    std::ofstream outfile;
+	infile.open("/home/kurt/github/CSVtoJSON/src/PER_data.csv");
+    std::vector<std::string> header = get_header(infile);
+    std::string out = "";
+    converter(infile, outfile, header);
 }
